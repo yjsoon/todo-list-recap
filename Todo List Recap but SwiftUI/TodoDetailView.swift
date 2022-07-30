e1e215c43a02a907d25d43073b28593371f126b1
YJ Soon <yjsoon@tinkertanker.com>
2022-07-30 10:19:30 +0800

    Added TodoDetailView and NavigationLink

---
+++ Todo List Recap but SwiftUI/TodoDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  TodoDetailView.swift
+//  Todo List Recap but SwiftUI
+//
+//  Created by YJ Soon on 30/7/22.
+//
+
+import SwiftUI
+
+struct TodoDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TodoDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoDetailView()
+    }
+}
