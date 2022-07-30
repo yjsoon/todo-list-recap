664b3a1be2dd82b5bf2e6bb2a7310d1da9229a9f
YJ Soon <yjsoon@tinkertanker.com>
2022-07-30 09:51:02 +0800

    Initial Commit

---
+++ Todo List Recap but SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo List Recap but SwiftUI
+//
+//  Created by YJ Soon on 30/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo List Recap but SwiftUI/Todo_List_Recap_but_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_List_Recap_but_SwiftUIApp.swift
+//  Todo List Recap but SwiftUI
+//
+//  Created by YJ Soon on 30/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo_List_Recap_but_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
